f4f6e5f474a37c6c6a25545c6fd09b846eaf6192
Henry <hgarc014@ucr.edu>
2016-09-16 22:30:20 -0700

    messing with layout for caraousel

--- app/www/courses/courses.js
+++ app/www/courses/courses.js
@@ -15,8 +15,8 @@ angular.module('myApp.courses', ['ngRoute'])
 
         var getElementPos = function (array, elem, elemVal) {
             var pos = -1;
-            for(var i in array){
-                if(array[i][elem] === elemVal){
+            for (var i in array) {
+                if (array[i][elem] === elemVal) {
                     pos = i;
                     break;
                 }
@@ -58,40 +58,74 @@ angular.module('myApp.courses', ['ngRoute'])
                 return;
             }
 
-        }
+        };
+
+        var addGPA =  function(year,gpa){
+            var yearsPos = helper.getElementPos($scope.years, "year", year);
+            var yearsObj = $scope.years[yearsPos];
+
+            yearsObj.gpa = gpa;
+        };
 
         $scope.years = [];
-        var sea = {F:"Fall",S:"Spring",W:"Winter",U:"Summer"};
+        var sea = {F: "Fall", S: "Spring", W: "Winter", U: "Summer"};
 
-        addCourse("2016",sea.S,"CS 179G: Database Systems");
-        addCourse("2016",sea.S,"CS 180: Intro to Software Engineering");
-        addCourse("2016",sea.W,"CS 150: Automata and Formal Languages");
-        addCourse("2016",sea.W,"CS 161: Design Architecture of Computer Systems");
-        addCourse("2016",sea.W,"CS 166: Database Management Systems");
-        addCourse("2016",sea.W,"STAT 155: PROBABLTY & STATSTCS");
+        addCourse("2016 - 2017", sea.F, "ENGR180W: Technical Communications");
+        addCourse("2016 - 2017", sea.F, "ANTH001: Cultural Anthropology");
+        addCourse("2016 - 2017", sea.F, "ECON003: Introduction to Microeconomics");
+        addCourse("2016 - 2017", sea.F, "HIST015: World History: 1500 to 1900");
+        addCourse("2016 - 2017", sea.W, "Coming Soon");
+        addCourse("2016 - 2017", sea.S, "Coming Soon");
+        addGPA("2016 - 2017",3.828);
 
-        addCourse("2015",sea.F,"CS 164: Computer Networks");
-        addCourse("2015",sea.F,"CS 165: Computer Security");
-        addCourse("2015",sea.S,"CS 153: Design of Operating Systems");
-        addCourse("2015",sea.S,"CS 120B: Introduction to Embedded Systems");
-        addCourse("2015",sea.W,"CS 172: Introduction to Data Retrieval");
-        addCourse("2015",sea.W,"CS 120A: Logic Design");
 
-        addCourse("2014", sea.F, "CS 141: Intermediate Data Structures and Algorithms");
-        addCourse("2014", sea.F, "CS 100: Software Construction");
-        addCourse("2014", sea.U, "CS 61: Machine Organization and Assembly Language Programming");
-        addCourse("2014", sea.S, "CS 14: Introduction to Data Structures and Algorithms");
-        addCourse("2014", sea.S, "CS 111: Discrete Structures");
-        addCourse("2014", sea.W, "CS 11: Introduction to Discrete Structures");
-        addCourse("2014", sea.W, "CS 12: Introduction to Computer Science, Mathematics, and Engineers Part 2");
+        //3.828
+        addCourse("2015 - 2016", sea.F, "CS 164: Computer Networks");
+        addCourse("2015 - 2016", sea.F, "CS 165: Computer Security");
+        addCourse("2015 - 2016", sea.F, "BIOL002: Cellular Basis of Life");
+        addCourse("2015 - 2016", sea.F, "ENGR101I: Professional Development and Mentoring");
+        addCourse("2015 - 2016", sea.W, "CS 150: Automata and Formal Languages");
+        addCourse("2015 - 2016", sea.W, "CS 161: Design Architecture of Computer Systems");
+        addCourse("2015 - 2016", sea.W, "CS 161L: Laboratory in Design and Architecture of Computer Systems");
+        addCourse("2015 - 2016", sea.W, "CS 166: Database Management Systems");
+        addCourse("2015 - 2016", sea.W, "STAT 155: PROBABLTY & STATSTCS");
+        addCourse("2015 - 2016", sea.S, "CS 179G: Database Systems");
+        addCourse("2015 - 2016", sea.S, "CS 180: Intro to Software Engineering");
+        addCourse("2015 - 2016", sea.S, "EE 020: Linear Methods for Engineering Analysis and Design Using MATLAB");
+        addGPA("2015 - 2016",3.828);
 
-        addCourse("2013", sea.F, "CS 10: Introduction to Computer Science for Science, Mathematics, and Engineers Part 1");
 
-        // addCourse("","","");
-        // addCourse("","","");
-        // addCourse("","","");
+        //3.814
+        addCourse("2014 - 2015", sea.F, "CS 141: Intermediate Data Structures and Algorithms");
+        addCourse("2014 - 2015", sea.F, "CS 100: Software Construction");
+        addCourse("2014 - 2015", sea.F, "MATH031: Applied Linear Algebra");
+        addCourse("2014 - 2015", sea.W, "CS 172: Introduction to Data Retrieval");
+        addCourse("2014 - 2015", sea.W, "CS 120A: Logic Design");
+        addCourse("2014 - 2015", sea.W, "PHIL124: Formal Logic");
+        addCourse("2014 - 2015", sea.S, "CS 153: Design of Operating Systems");
+        addCourse("2014 - 2015", sea.S, "CS 120B: Introduction to Embedded Systems");
+        addCourse("2014 - 2015", sea.S, "PHYS040C: General Physics");
+        addGPA("2014 - 2015",3.814);
 
-        // console.log($scope.years);
+
+
+        //3.717
+        addCourse("2013 - 2014", sea.F, "CS 10: Introduction to Computer Science for Science, Mathematics, and Engineers Part 1");
+        addCourse("2013 - 2014", sea.F, "ENGL001A: Beginning Composition");
+        addCourse("2013 - 2014", sea.F, "ENGR001I: Professional Development and Mentoring");
+        addCourse("2013 - 2014", sea.F, "MATH009C: 	First-Year Calculus");
+        addCourse("2013 - 2014", sea.W, "CS 11: Introduction to Discrete Structures");
+        addCourse("2013 - 2014", sea.W, "CS 12: Introduction to Computer Science, Mathematics, and Engineers Part 2");
+        addCourse("2013 - 2014", sea.W, "MATH010A: 	Calculus of Several Variables");
+        addCourse("2013 - 2014", sea.W, "ENGL001B: 	Intermediate Composition");
+        addCourse("2013 - 2014", sea.S, "CS 14: Introduction to Data Structures and Algorithms");
+        addCourse("2013 - 2014", sea.S, "CS 111: Discrete Structures");
+        addCourse("2013 - 2014", sea.S, "MATH046: 	Introduction to Ordinary Differential Equations");
+        addCourse("2013 - 2014", sea.U, "CS 61: Machine Organization and Assembly Language Programming");
+        addCourse("2013 - 2014", sea.U, "PHYS040A: General Physics");
+        addCourse("2013 - 2014", sea.U, "PHYS040B: General Physics");
+        addGPA("2013 - 2014",3.717);
+
 
     })
 
--- app/www/stories/stories.js
+++ app/www/stories/stories.js
@@ -47,17 +47,44 @@ angular.module('myApp.stories', ['ngRoute', 'angular-carousel'])
 
         var gen = {"ACT": "Action", "FANT": "Fantasy", "HORR": "Horror"};
 
-        addStory(gen["ACT"], "Gun Shots", "We were just making a quick pitstop at a gas station before we continued our long drive. It wasn't long before we decided to head out, but that's when we were first attacked.", "myLink");
+        addStory(gen["ACT"], "Gun Shots", "We were just making a quick pitstop at a gas station before we continued our long drive. " +
+            "It wasn't long before we decided to head out, but that's when we were first attacked.",
+            "https://docs.google.com/document/d/1bX25QHNcO7Dx5sPb_ITRcFJ-WTaI8inGs1Oh7MFulTc/edit");
 
-        addStory(gen["FANT"], "Psychic Powers", "My name is Jake Smith I freed my people from the burden of humanity. With the help of some staffs left by my ancestors I was able to return sorcery to my people. Now we face a large threat that my ancestors had failed to defeat.");
+        addStory(gen["FANT"], "Psychic Powers", "My name is Jake Smith I freed my people from the burden of humanity. " +
+            "With the help of some staffs left by my ancestors I was able to return sorcery to my people. " +
+            "Now we face a large threat that my ancestors had failed to defeat.",
+            "https://docs.google.com/document/d/1565JhXcdTbCdtlPIQk-fYMJ9CE57znCTKXh6RjyfjMA/edit");
 
-        addStory(gen["HORR"], "Ghost Children", "I never really liked driving around with my sister, even before she had a learners permit. I thought we were going to go for a little drive around time and come home for dinner, but boy was I wrong. Out of no where I end up in home with ghosts and an evil witch. I have no idea what lies ahead.", "myLink");
-        addStory(gen["HORR"], "Half Human Robot", "John and William try to survive the ever growing human robots that are taking over the world. They both get captured and are placed in a prison cell. They are faced with escaping or becoming another human robot to assist taking over the world.", "myLink");
-        addStory(gen["HORR"], "Magical House Dungeon", "My brother and I woke up in this random isolated room with no escape. We have to find a way out of here. I don't want to find out what hides inside these walls.", "myLink");
-        addStory(gen["HORR"], "Mizuki", "A person visits his grandmother's house to find himself messing with evil cards. It doesn't take long after playing with the wrong cards to summon Mizuki, placing him and his family in deep trouble.", "myLink");
-        addStory(gen["HORR"], "Suicide Cup", "A person who finds himself at a old ladies home with an evil forest. When the day turns dark unknown species appears.", "myLink");
-        addStory(gen["HORR"], "Sun God", "What harm can watching a movie have? A college student finds an old movie with his family that he had found. Soon after watching the movie him and his family are sucked into the movie. They have to survive fire dogs and get out of the movie and stop it in order to save their lives and the lives of the world.", "myLink");
-        addStory(gen["HORR"], "Zombie Story", "My name is Garry. Before all of the madness started I was a janitor. My job wasn't all that appealing or easy, but you kind of get used to it. In this new hell of a world I wouldn't say you ever get used to it, especially at night.", "myLink");
+        addStory(gen["HORR"], "Ghost Children", "I never really liked driving around with my sister, even before she had a learners permit. " +
+            "I thought we were going to go for a little drive around time and come home for dinner, but boy was I wrong. " +
+            "Out of no where I end up in home with ghosts and an evil witch. I have no idea what lies ahead.",
+            "https://docs.google.com/document/d/1GVCujkC-DCGG3SavFO2llH5x8WbkYs5WOp6KNpuuG-s/edit");
+
+        addStory(gen["HORR"], "Half Human Robot", "John and William try to survive the ever growing human robots that are taking over the world. " +
+            "They both get captured and are placed in a prison cell. They are faced with escaping or becoming another human robot to assist taking over the world.",
+            "https://docs.google.com/document/d/1SCB7Vsiohzv2-2U_h_PWdZZaYZs_0RpgNR5e8jxhz34/edit");
+
+        addStory(gen["HORR"], "Magical House Dungeon", "My brother and I woke up in this random isolated room with no escape. " +
+            "We have to find a way out of here. I don't want to find out what hides inside these walls.",
+            "https://docs.google.com/document/d/1uKLdbT2bKY6rCddX0syoxrsochbQGS81UexBm9ELnLc/edit");
+
+        addStory(gen["HORR"], "Mizuki", "A person visits his grandmother's house to find himself messing with evil cards. " +
+            "It doesn't take long after playing with the wrong cards to summon Mizuki, placing him and his family in deep trouble.",
+            "https://docs.google.com/document/d/1y8yKfzhc3kAFmqT2uus4ruxvUyQJFsA6HxMOqvgVPh4/edit");
+
+        addStory(gen["HORR"], "Suicide Cup", "A person who finds himself at a old ladies home with an evil forest. " +
+            "When the day turns dark unknown species appears.",
+            "https://docs.google.com/document/d/1xU-XxNh3424v71WXw5YV5M7yqCfU1s2pU8tWfvJk65o/edit");
+
+        addStory(gen["HORR"], "Sun God", "What harm can watching a movie have? " +
+            "A college student finds an old movie with his family that he had found. Soon after watching the movie him and his family are sucked into the movie. " +
+            "They have to survive fire dogs and get out of the movie and stop it in order to save their lives and the lives of the world.",
+            "https://docs.google.com/document/d/1sEsZPiKA1fvpakLkmSMA3O3rQnAU-518mWboxIDWshY/edit");
+
+        addStory(gen["HORR"], "Zombie Story", "My name is Garry. Before all of the madness started I was a janitor. " +
+            "My job wasn't all that appealing or easy, but you kind of get used to it. In this new hell of a world I wouldn't say you ever get used to it, especially at night.",
+            "https://docs.google.com/document/d/1ALP90rLabK8R0WA7mPWMy88mEc9JM2Dy_sNmldw3OCU/edit");
 
         // console.log("GENRES:");
         // console.log($scope.genres);
